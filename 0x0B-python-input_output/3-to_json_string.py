5590cce676918adf054a60efbcf4c338309ef101
Yator1 <titusyatz17@gmail.com>
2024-01-09 10:41:39 -0800

    JSON rep of a string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This file contains one function
+That return the JSON represantation of an object(string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the json representation of an object
+
+    Args:
+        my_obj (str): the object to be converted to json
+
+    Return:
+        The JSON represantation of the object
+    """
+    json_rep = json.dumps(my_obj)
+
+    return (json_rep)
