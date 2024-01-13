b792fd2bde9c2825b3962da97d46ccb3ff4d4e25
Yator1 <titusyatz17@gmail.com>
2024-01-13 02:02:09 -0800

    Checking if same class or inherited

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,7 @@ This module contains one function {is_kind_of_class}
 
 def is_kind_of_class(obj, a_class):
     """
-    this function checks if an object is of a class or inherits from the same class
+    this function checks if an object is of a class or inherits from 
+    the same class
     """
     return (isinstance(obj, a_class))
