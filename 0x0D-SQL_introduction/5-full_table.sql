80c5d68e8bdc9fac9a734adc327d4985ccb2709c
Yator1 <titusyatz17@gmail.com>
2024-02-14 00:29:22 -0800

    Table description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Print the full description of the table first_table
+-- from hbtn_0c_0
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
