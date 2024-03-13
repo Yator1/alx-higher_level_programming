436242184a507283a73880cbda1b28664025155a
Yator1 <titusyatz17@gmail.com>
2024-03-13 13:14:58 -0700

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * script that prints two arguments passed to it in the format: 
+ * "is"
+ */
+const { argv } = require('process');
+
+if (argv[2] && argv[3]) {
+  console.log(argv[2] + ' is ' + argv[3]);
+} else if (argv[2]) {
+  console.log(argv[2] + ' is undefined');
+} else {
+  console.log('undefined is undefined');
+}
