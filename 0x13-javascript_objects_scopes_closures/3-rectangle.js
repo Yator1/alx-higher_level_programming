bc10692aafb6e989620d27cd82b7f71d91e28064
Yator1 <titusyatz17@gmail.com>
2024-03-23 16:56:23 +0300

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/* 
+ * Class Rectangle that defines a rectangle:
+ * An instance method prints the rectangle using the character X
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+
+    return constructor.name + {};
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
