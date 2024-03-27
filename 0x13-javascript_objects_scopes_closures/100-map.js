971a36631906189aadcc3a427b1775be3db166cb
Yator1 <titusyatz17@gmail.com>
2024-03-27 19:56:34 +0300

    Mapping

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// A script that imports an array and computes a new array.
+
+const { list } = require('./100-data');
+
+console.log(list);
+console.log(list.map((element, idx) => element * idx));
