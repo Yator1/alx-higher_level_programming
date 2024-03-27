35f5fe09cad381062e205b1d0fcb3878ff0d266a
Yator1 <titusyatz17@gmail.com>
2024-03-27 20:05:01 +0300

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* 
+ * A script that imports a dictionary of occurrences by user id and
+ * computes a dictionary of user ids by occurrence.
+ */
+const { dict } = require('./101-data');
+
+const newUserDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+    if (!newUserDict[occurrences]) {
+      newUserDict[occurrences] = [userId];
+    } else {
+      newUserDict[occurrences].push(userId);
+    }
+}
+
+console.log(newUserDict);
+
