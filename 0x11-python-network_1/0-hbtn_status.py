c731d8c8c87ea7d6cf379bedb053870bfdd75316
Yator1 <titusyatz17@gmail.com>
2024-03-31 02:52:08 -0700

    Char correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    url =  urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as resp:
         body = resp.read()
 
