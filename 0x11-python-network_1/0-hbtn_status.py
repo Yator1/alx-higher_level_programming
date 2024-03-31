a1bb136e76c4b43f7e74993a9dd20b1cfb42820d
Yator1 <titusyatz17@gmail.com>
2024-03-31 02:49:54 -0700

    Char correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as resp:
         body = resp.read()
 
-        print("- Body response:")
+        print("Body response:")
         print("      - type: {}".format(type(body)))
         print("      - content: {}".format(body))
         print("      - utf8 content: {}".format(body.decode("utf-8")))
