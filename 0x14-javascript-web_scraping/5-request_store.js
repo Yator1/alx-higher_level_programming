1c8eae85267c5b75fb6c9779e034ea953885f3bb
Yator1 <titusyatz17@gmail.com>
2024-04-25 00:55:34 -0700

    Style

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -11,12 +11,12 @@ const url = process.argv[2];
 const file = process.argv[3];
 
 request.get(url, (error, response, body) => {
-if (error) {
+  if (error) {
     console.log(error);
     return;
   }
 
-fs.writeFile(file, body, 'utf-8', err => {
+  fs.writeFile(file, body, 'utf-8', err => {
     if (err) {
       console.log(err);
     }
