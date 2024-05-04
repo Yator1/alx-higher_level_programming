6efaff3fd5d35a92de62918233f2ae327c500a8e
Yator1 <titusyatz17@gmail.com>
2024-05-04 14:24:00 +0300

    Fetching a name

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $.ajax({
+      url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+      type: 'GET',
+      success: function (data) {
+        $('#character').text(data.name);
+      }
+    });
+  });
